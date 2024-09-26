aaa4e90bfbc1617989e578e22921e258fba27b4f
rzfzr <capitao.rafa@gmail.com>
2024-09-26 09:42:03 -0300

    Adding content

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,39 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-  <div class="greetings">
-    <h1 class="text-green-300">Hello world</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
-  </div>
+
 </template>
+
+<script>
+
+export default {
+  components: {
+  },
+  data() {
+    return {
+      selectedTab: 'Data Centers',
+      tabs: ['Data Centers', 'Industrial', 'Power Management', 'Wireless Enclosures'],
+      tabContent: {
+        'Data Centers': {
+          title: 'Data Center & Enterprise Solutions',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vehicula commodo pharetra. Praesent pharetra velit massa, ac vehicula lorem sollicitudin nec. Maecenas ligula nisl, pretium sed eleifend at, gravida eu dui. Praesent vestibulum velit id congue congue. ',
+          imageUrl: 'src/assets/data-centers.png'
+        },
+        'Industrial': {
+          title: 'Industrial Solutions',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vehicula commodo pharetra. Praesent pharetra velit massa, ac vehicula lorem sollicitudin nec. Maecenas ligula nisl, pretium sed eleifend at, gravida eu dui. Praesent vestibulum velit id congue congue. ',
+          imageUrl: 'src/assets/industrial-solutions.png'
+        },
+        'Power Management': {
+          title: 'Industrial Solutions',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vehicula commodo pharetra. Praesent pharetra velit massa, ac vehicula lorem sollicitudin nec. Maecenas ligula nisl, pretium sed eleifend at, gravida eu dui. Praesent vestibulum velit id congue congue. ',
+          imageUrl: 'src/assets/power-management.png'
+        },
+        'Wireless Enclosures': {
+          title: 'Wireless Enclosures',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vehicula commodo pharetra. Praesent pharetra velit massa, ac vehicula lorem sollicitudin nec. Maecenas ligula nisl, pretium sed eleifend at, gravida eu dui. Praesent vestibulum velit id congue congue. ',
+          imageUrl: 'src/assets/wireless-enclosures.png'
+        },
+      }
+    }
+  }
+}
+</script>
