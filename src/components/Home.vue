a305e2ac5936aa10a8b85a18e760bc6d31e88035
rzfzr <capitao.rafa@gmail.com>
2024-09-26 09:28:39 -0300

    Removing unused

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="text-green-300">Hello world</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+    </h3>
+  </div>
+</template>
+
+
