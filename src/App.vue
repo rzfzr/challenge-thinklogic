871c0afcb48e5c97d1ea88d8568c22c2cb148174
rzfzr <capitao.rafa@gmail.com>
2024-09-26 10:19:43 -0300

    Adding set padding for screen sizes

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,8 @@ import Home from './components/Home.vue'
   </header>
 
   <main>
-    <Home />
+    <div class="lg:px-[90px] lg:py-[100px] md:px-[40px] md:py-[60px] px-[32px] py-[20px]">
+      <Home />
+    </div>
   </main>
 </template>
